289dcc9e1cb64c664a0d25b400522e5ec514e651
Akpos-Ben <deltapengineeringservices@gmail.com>
2024-04-27 06:27:16 -0700

    Done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Uses the GitHub API to display a GitHub ID based on given credentials.
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
-- Uses Basic Authentication to access the ID.
+  - Uses Basic Authentication to access the ID.
 """
 import sys
 import requests
@@ -9,6 +9,6 @@ from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-r = requests.get("https://api.github.com/user", auth=auth)
-print(r.json().get("id"))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
